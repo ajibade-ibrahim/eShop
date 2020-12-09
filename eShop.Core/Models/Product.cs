a723399179ff7ae2e636e068ebd8e0a90b0fda3a
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-09 10:10:30 +0100

    Added Product description property

--- eShop.Core/Models/Product.cs
+++ eShop.Core/Models/Product.cs
@@ -6,6 +6,7 @@
         public int Id { get; set; }
         public string ImageUrl { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public double Price { get; set; }
     }
 }
