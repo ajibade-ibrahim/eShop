8dd632a74101fb9c49a8749a381c547932ff6a0b
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-09 09:47:17 +0100

    Added Product model class

---
+++ eShop.Core/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace eShop.Core.Models
+{
+    public class Product
+    {
+        public string Brand { get; set; }
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
