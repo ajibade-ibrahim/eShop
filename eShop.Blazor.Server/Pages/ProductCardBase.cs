81c1ea0f5e4319e73c2dfea99e3d127beb68e65e
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-10 14:50:58 +0100

    Implemented ProductCard component

---
+++ eShop.Blazor.Server/Pages/ProductCardBase.cs
@@ -0,0 +1,11 @@
+﻿using eShop.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace eShop.Blazor.Server.Pages
+{
+    public class ProductCardBase : ComponentBase
+    {
+        [Parameter]
+        public Product Product { get; set; }
+    }
+}
