8f5b75187492ca81a3f497f3646698d275e27971
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-10 17:31:27 +0100

    Implemented ProductDetail page

---
+++ eShop.Blazor.Server/Pages/ProductDetailBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using eShop.Core.Models;
+using eShop.UseCases.SearchProductsScreen.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace eShop.Blazor.Server.Pages
+{
+    public class ProductDetailBase : ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        [Inject]
+        public IViewProduct ViewProductService { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            try
+            {
+                Product = await ViewProductService.Execute(Id);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
