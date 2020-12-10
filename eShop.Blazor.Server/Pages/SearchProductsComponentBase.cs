40a770676dc7fb3ab1836b427227fed38f8e3bf6
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-10 09:48:55 +0100

    Created SearchProducts component and integrated template page

---
+++ eShop.Blazor.Server/Pages/SearchProductsComponentBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShop.Core.Models;
+using eShop.UseCases.SearchProductsScreen.Contracts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace eShop.Blazor.Server.Pages
+{
+    public class SearchProductsComponentBase : ComponentBase
+    {
+        [Inject]
+        public ISearchProduct SearchProductService { get; set; }
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+        public List<Product> Products { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                var products = await SearchProductService.Execute(string.Empty);
+                Products = products?.ToList() ?? new List<Product>();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            JsRuntime.InvokeVoidAsync("initialize");
+            return base.OnAfterRenderAsync(firstRender);
+        }
+    }
+}
--- eShop.Blazor.Server/Startup.cs
+++ eShop.Blazor.Server/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using eShop.Blazor.Server.Data;
+using eShop.Data.Static;
+using eShop.UseCases.Contracts.Data;
+using eShop.UseCases.SearchProductsScreen;
+using eShop.UseCases.SearchProductsScreen.Contracts;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using eShop.Blazor.Server.Data;
 
 namespace eShop.Blazor.Server
 {
@@ -22,15 +20,6 @@ namespace eShop.Blazor.Server
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -41,6 +30,7 @@ namespace eShop.Blazor.Server
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,11 +40,24 @@ namespace eShop.Blazor.Server
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
-            });
+            app.UseEndpoints(
+                endpoints =>
+                {
+                    endpoints.MapBlazorHub();
+                    endpoints.MapFallbackToPage("/_Host");
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ISearchProduct, SearchProduct>();
+            services.AddScoped<IViewProduct, ViewProduct>();
         }
     }
 }
