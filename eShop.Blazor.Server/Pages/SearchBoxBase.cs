dbcbfe2b5a74f14f32013b1f2f0eeeba126a72e3
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-15 14:25:56 +0100

    Extracted SearchBox to component

--- eShop.Blazor.Server/Pages/SearchBoxBase.cs
+++ eShop.Blazor.Server/Pages/SearchBoxBase.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 namespace eShop.Blazor.Server.Pages
 {
@@ -8,21 +7,5 @@ namespace eShop.Blazor.Server.Pages
         [Parameter]
         public EventCallback<string> HandleSearch { get; set; }
         public string SearchText { get; set; }
-
-        protected void HandleKeyUp(KeyboardEventArgs eventArgs)
-        {
-            if (eventArgs.Code == "Enter" || eventArgs.Code == "NumpadEnter")
-            {
-                // ...
-            }
-        }
-
-        protected void OnSearchSubmit()
-        {
-            if (!string.IsNullOrWhiteSpace(SearchText))
-            {
-                HandleSearch.InvokeAsync(SearchText);
-            }
-        }
     }
 }
